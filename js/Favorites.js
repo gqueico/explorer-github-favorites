e902fc39a95b87cfef7efe0b5ba8eb24bad554ca
gqueico <gqueico@gmail.com>
2024-08-20 07:38:48 -0300

    :hammer: Create Favorites.js and GithubUser.js

--- js/Favorites.js
+++ js/Favorites.js
@@ -1,29 +1,40 @@
-export class GithubUser {
-  static search(username) {
-    const endpoint = `https://api.github.com/users/${username}`
-
-    return fetch(endpoint)
-    .then(data => data.json())
-    .then(({ login, name, public_repos, followers }) => ({
-      login,
-      name,
-      public_repos,
-      followers
-    }))
-  }
-}
-
+import { GithubUser } from './GithubUser.js'
 
 export class Favorites {
   constructor(root) {
     this.root = document.querySelector(root)
     this.load()
-
-    GithubUser.search('gqueico').then(user => console.log(user))
   }
 
   load() {
     this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
+  }
+
+  save() {
+    localStorage.setItem('@github-favorites:', JSON.stringify(this.entries))
+  }
+
+  async add(username) {
+    try {
+      const userExists = this.entries.find(entry => entry.login === username)
+
+      if(userExists) {
+        throw new Error('Usuário já cadastrado!')
+      }
+
+      const user = await GithubUser.search(username)
+
+      if(user.login === undefined) {
+        throw new Error('Usuário não encontrado!')
+      }
+
+      this.entries = [user, ...this.entries]
+      this.update()
+      this.save()
+
+    } catch(error) {
+      alert(error.message)
+    }
   }
 
   delete(user) {
@@ -31,6 +42,7 @@ export class Favorites {
 
     this.entries = filteredEntries
     this.update()
+    this.save()
   }
 }
 
@@ -44,7 +56,14 @@ export class FavoritesView extends Favorites {
     this.onadd()
   }
 
-  onadd(){}
+  onadd(){
+    const addButton = this.root.querySelector('.search button')
+    addButton.onclick = () => {
+      const { value } = this.root.querySelector('.search input')
+
+      this.add(value)
+    }
+  }
 
   update() {
     this.removeAllTr()
